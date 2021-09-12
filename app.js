095549338622427b61f756858647a676983c679f
learnwithkawsar <kawsar721g@gmail.com>
2021-09-12 10:44:37 +0600

    done

---
+++ app.js
@@ -0,0 +1,22 @@
+const myClock = document.getElementById('myClock');
+function time(){
+    const date = new Date();
+    let h = date.getHours();
+    let m = date.getMinutes();
+    let s = date.getSeconds();
+    let session ='AM';
+    if(h==0){
+        h=12;
+    }
+    if(h>12){
+        h = h-12;
+        session= 'PM';
+    }
+    h = (h<10)? '0' + h : h;
+    m = (m<10)? '0' + m : m;
+    s = (s<10)? '0' + s : s;
+    myClock.innerHTML=`${h}:${m}:${s} ${session}`;
+    // console.log(h,m,s,session);
+}
+setInterval(time,1000);
+// time();
