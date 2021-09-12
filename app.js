4f733fb3cb8f5cca6bd2943144444809c1e86ebb
learnwithkawsar <kawsar721g@gmail.com>
2021-09-12 12:33:22 +0600

    error fixt

--- app.js
+++ app.js
@@ -5,12 +5,12 @@ function time(){
     let m = date.getMinutes();
     let s = date.getSeconds();
     let session ='AM';
-    if(h==0){
-        h=12;
-    }
-    if(h>12){
+    if(h>=12){
         h = h-12;
         session= 'PM';
+    }
+    if(h===0){
+        h=12;
     }
     h = (h<10)? '0' + h : h;
     m = (m<10)? '0' + m : m;
